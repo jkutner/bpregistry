f434762da05f42b70d8bdcdb4ada0f3fddb9e9a7
Joe Kutner <jpkutner@gmail.com>
2019-08-09 13:19:22 -0500

    routes

--- main.go
+++ main.go
@@ -16,19 +16,21 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/v2/*repo", func(c *gin.Context) {
-		target := c.Request.URL
-		target.Scheme = "https"
-		target.Host = "registry.hub.docker.com"
-		log.Info(target.String())
-		c.Redirect(http.StatusMovedPermanently, target.String())
+	r.GET("/v2/", func(c *gin.Context) {
+		redirectToDockerhub(c)
 	})
-	r.HEAD("/v2/*repo", func(c *gin.Context) {
-		target := c.Request.URL
-		target.Scheme = "https"
-		target.Host = "registry.hub.docker.com"
-		log.Info(target.String())
-		c.Redirect(http.StatusMovedPermanently, target.String())
+	r.GET("/v2/:namespace/*repo", func(c *gin.Context) {
+		redirectToDockerhub(c)
+	})
+	r.HEAD("/v2/:namespace/*repo", func(c *gin.Context) {
+		redirectToDockerhub(c)
 	})
 	_ = r.Run(":" + os.Getenv("PORT"))
+}
+
+func redirectToDockerhub(c *gin.Context) {
+	target := c.Request.URL
+	target.Scheme = "https"
+	target.Host = "registry.hub.docker.com"
+	c.Redirect(http.StatusMovedPermanently, target.String())
 }
